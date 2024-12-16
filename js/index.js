4c33804a690bbbc8b0a5e84b8922a4428b2ed23b
leehee0930 <ljgkkb@gmail.com>
2024-12-16 12:48:13 +0900

    gnb수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-  $(".main").hover(function(){
-    $(this).find(".sub").stop().slideDown();
-  },function(){
-    $(this).find(".sub").stop().slideUp();
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".gnb li");
+  const currentPath = window.location.pathname.split("/").pop(); // 현재 파일명 추출
+
+  menuItems.forEach((item) => {
+    const link = item.querySelector("a").getAttribute("href");
+    if (link === currentPath) {
+      item.classList.add("active"); // URL과 href가 일치하면 active 추가
+    } else {
+      item.classList.remove("active"); // URL과 href가 일치하지 않으면 active 제거
+    }
   });
-}); */
+});
